3ec863569122d0d1e347fc52448ac50a20e95f58
chrisbusche26 <chrisbusche26@gmail.com>
2024-06-10 05:48:52 -0500

    GA4 task one complete, task two ongoing

---
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/EmployerRepository.java
@@ -0,0 +1,10 @@
+package org.launchcode.techjobs.persistent.models.data;
+
+import org.launchcode.techjobs.persistent.models.Job;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployerRepository extends CrudRepository<Job, Integer> {
+
+}
